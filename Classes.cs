14b3b1cf9eae8799e6ebefd8e6708c4f3b16e9ea
Muhammad Huzaifa Shah <43364469+Huzaif-360@users.noreply.github.com>
2020-04-14 00:18:27 +0500

    Classes (Non- Primitive Type)

---
+++ Classes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string fName;
+        public string lName;
+    public void Introduce()
+        {
+            Console.WriteLine("my name is " + fName + " " + lName);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();  //var= Person
+            person.fName = "Huzaifa";
+            person.lName = "Shah";
+            person.Introduce();
+        }
+    }
+}
