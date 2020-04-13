9c1e3b3efff3701b8a51a6eecb2c43bbb6d9d031
Muhammad Huzaifa Shah <43364469+Huzaif-360@users.noreply.github.com>
2020-04-14 00:23:51 +0500

    Strings (Non- Primitive Type)

---
+++ Strings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Huzaifa";
+            var lastName = "Shah";
+            var fullName1 = name + " " + lastName;                                  //M1
+            Console.WriteLine(fullName1);
+            var fullName2 = string.Format("{0} {1}  " , name , lastName);           //M2
+            Console.WriteLine(fullName2);
+
+
+        }
+    }
+}
