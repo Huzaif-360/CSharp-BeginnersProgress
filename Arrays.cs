3b21aaa0ca17ae7bb82ac79f346bdb9183f1381f
Muhammad Huzaifa Shah <43364469+Huzaif-360@users.noreply.github.com>
2020-04-14 00:22:07 +0500

    Arrays (Non- Primitive Type)

---
+++ Arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[3];
+            number[0] = 1;
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]);
+            Console.WriteLine(number[2]);
+            //.................................................................
+            var check = new bool[3];
+            check[0] = true;
+            Console.WriteLine(check[0]);
+            Console.WriteLine(check[1]);
+            Console.WriteLine(check[2]);
+            //.................................................................
+            var names = new string[3] { "Huzaifa", "Nouman", "Saad" };
+            //names[0] = "Huzaifa";
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+        }
+    }
+}
