705c758f6ad7cd6e6a824662f713786988c59ef1
Muhammad Huzaifa Shah <43364469+Huzaif-360@users.noreply.github.com>
2020-04-14 00:07:18 +0500

    Type Conversion
    Demo of Type Conversion from Section 3

---
+++ Type_Conversion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+            int c = 256; 
+            byte d = (byte)c;
+            Console.WriteLine(d);
+            // Non- Compatible conversitons.
+            var s = "1234";
+            var j = Convert.ToInt32(s);
+            Console.WriteLine(j);
+
+            // Exception Handling
+            /* var number = "1234";
+             var bbb= Convert.ToByte(number);
+             Console.WriteLine(bbb);
+             */
+            try
+            {
+                var number = "1234";
+                var bbb = Convert.ToByte(number);
+                Console.WriteLine(bbb);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("This conversion can not be done due to less space in byte!!!");
+
+            }
+            //....................... OPERATORS ..............................
+            var z = 1;
+            var y = 2;
+            Console.WriteLine("a+b = "+ z+y);
+            Console.WriteLine("a/b = "+ z / y);
+            Console.WriteLine("a/b = "+ (float)z / (float) y);
+            Console.WriteLine("a>b = " + (z > y));
+            Console.WriteLine("a==b = "+(z == y));
+            Console.WriteLine("a<b = "+(z < y));
+
+
+
+        }
+    }
+}
