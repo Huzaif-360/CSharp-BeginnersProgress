cd28e49a0dff7b82753183da5e439fcb8a246bbe
Muhammad Huzaifa Shah <43364469+Huzaif-360@users.noreply.github.com>
2020-04-13 23:41:36 +0500

    First C# Application

---
+++ Helloworld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.WriteLine("Hello World 2!");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            */
+            var number = 1;
+           var count = 3;
+           var hj = 2.11f;
+
+            Console.WriteLine(number); 
+            Console.WriteLine(hj);
+
+        }
+    }
+}
